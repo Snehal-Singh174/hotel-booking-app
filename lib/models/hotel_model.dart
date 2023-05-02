ef85f0f6acc34ea93ad6949657a8ce72af9873ba
Snehal Singh <53808476+Snehal-Singh174@users.noreply.github.com>
2023-05-02 22:30:04 +0530

    working on hotel repo

--- lib/models/hotel_model.dart
+++ lib/models/hotel_model.dart
@@ -6,18 +6,20 @@ class HotelModel {
   final String location;
   final String address;
   final String description;
+  final String thumbnailPath;
   final List<String> imagePaths;
   final int totalReview;
   final double ratingScore;
   final double price;
   final LatLng coordinate;
 
-  HotelModel({
+  const HotelModel({
     required this.id,
     required this.title,
     required this.location,
     required this.address,
     required this.description,
+    required this.thumbnailPath,
     required this.imagePaths,
     required this.price,
     required this.coordinate,
@@ -25,5 +27,54 @@ class HotelModel {
     this.ratingScore = 0,
   });
 
-  static List<HotelModel> sampleHotel = const [];
+  static List<HotelModel> sampleHotel = const [
+    HotelModel(
+        id: '1',
+        thumbnailPath: 'assets/image/thumbnail2.png',
+        title: 'Alana Hotel',
+        location: 'Bantul Regency, Yogyakarta',
+        address: 'Jl. Progratris 8.5 km, Yogyakarta 55186',
+        description: 'We are only 10 minute drive from the water castle',
+        imagePaths: ['assets/image/gallery1.png', 'assets/image/gallery1.png', 'assets/image/gallery1.png'],
+        price: 123,
+        coordinate: LatLng(-7.81478719, 110.3692165),
+        ratingScore: 10
+    ),
+    HotelModel(
+        id: '2',
+        thumbnailPath: 'assets/image/thumbnail2.png',
+        title: 'Alana Hotel',
+        location: 'Bantul Regency, Yogyakarta',
+        address: 'Jl. Progratris 8.5 km, Yogyakarta 55186',
+        description: 'We are only 10 minute drive from the water castle',
+        imagePaths: ['assets/image/gallery1.png', 'assets/image/gallery1.png', 'assets/image/gallery1.png'],
+        price: 123,
+        coordinate: LatLng(-7.81478719, 110.3692165),
+        ratingScore: 10
+    ),
+    HotelModel(
+        id: '3',
+        thumbnailPath: 'assets/image/thumbnail2.png',
+        title: 'Alana Hotel',
+        location: 'Bantul Regency, Yogyakarta',
+        address: 'Jl. Progratris 8.5 km, Yogyakarta 55186',
+        description: 'We are only 10 minute drive from the water castle',
+        imagePaths: ['assets/image/gallery1.png', 'assets/image/gallery1.png', 'assets/image/gallery1.png'],
+        price: 123,
+        coordinate: LatLng(-7.81478719, 110.3692165),
+        ratingScore: 10
+    ),
+    HotelModel(
+        id: '4',
+        thumbnailPath: 'assets/image/thumbnail2.png',
+        title: 'Alana Hotel',
+        location: 'Bantul Regency, Yogyakarta',
+        address: 'Jl. Progratris 8.5 km, Yogyakarta 55186',
+        description: 'We are only 10 minute drive from the water castle',
+        imagePaths: ['assets/image/gallery1.png', 'assets/image/gallery1.png', 'assets/image/gallery1.png'],
+        price: 123,
+        coordinate: LatLng(-7.81478719, 110.3692165),
+        ratingScore: 10
+    ),
+  ];
 }
---
+++ lib/providers/all_hotels_provider.dart
@@ -0,0 +1,11 @@
+import 'package:hotel_booking_app/models/hotel_model.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import '../repositories/hotel_repository.dart';
+
+part 'all_hotels_provider.g.dart';
+
+@riverpod
+Future<List<HotelModel>> allHotels(AllHotelsRef ref) async {
+  final HotelRepository hotelRepository = ref.watch(hotelRepositoryProvider);
+  return hotelRepository.getHotels();
+}
